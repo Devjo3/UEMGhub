953f0ad74b3be9da62aa3f7ffe29fb66a5f586f4
Devjo3 <bufalloxs@gmail.com>
2023-12-14 01:34:58 -0300

    enviar msg com Enter

--- scripts.js
+++ scripts.js
@@ -1,58 +1,65 @@
-  // Incorporação do código do scripts.js
+document.addEventListener("DOMContentLoaded", function() {
+  const minimizeButton = document.getElementById("minimize-button");
+  const chatMessages = document.getElementById("chat-messages");
+  const messageInput = document.getElementById("message-input"); // Adicionado o elemento do campo de mensagem
 
-  document.addEventListener("DOMContentLoaded", function() {
-    const minimizeButton = document.getElementById("minimize-button");
-    const chatMessages = document.getElementById("chat-messages");
-
-    minimizeButton.addEventListener("click", function() {
-      chatMessages.classList.toggle("hidden");
-    });
-
-    loadMessages(); // Carregar as mensagens quando a página é carregada
-    setInterval(loadMessages, 1000); // Atualizar as mensagens a cada segundo
+  minimizeButton.addEventListener("click", function() {
+    chatMessages.classList.toggle("hidden");
   });
 
-  function loadMessages() {
-    const chatMessages = document.getElementById("chat-messages");
-    // Fazer uma solicitação AJAX para buscar mensagens do servidor
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", "get_messages.php", true);
-    xhr.onload = function() {
-      if (xhr.status === 200) {
-        chatMessages.innerHTML = xhr.responseText;
-        chatMessages.scrollTop = chatMessages.scrollHeight;
-      }
-    };
-    xhr.send();
-  }
+  loadMessages(); // Carregar as mensagens quando a página é carregada
+  setInterval(loadMessages, 1000); // Atualizar as mensagens a cada segundo
 
-  function sendMessage() {
-    const messageInput = document.getElementById("message-input");
-    const message = messageInput.value.trim();
-    if (message !== "") {
-      // Enviar mensagem para o servidor usando AJAX
-      const xhr = new XMLHttpRequest();
-      xhr.open("POST", "send_message.php", true);
-      xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-      xhr.send("message=" + encodeURIComponent(message));
-      messageInput.value = "";
+  // Event listener para enviar a mensagem ao pressionar Enter
+  messageInput.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+      event.preventDefault(); // Evitar a quebra de linha padrão
+      sendMessage();
     }
-  }
+  });
+});
 
-  // Função para abrir a modal
-  function openModal() {
-    var modal = document.getElementById("myModal");
-    modal.style.display = "block";
-  }
+function loadMessages() {
+  const chatMessages = document.getElementById("chat-messages");
+  // Fazer uma solicitação AJAX para buscar mensagens do servidor
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", "get_messages.php", true);
+  xhr.onload = function() {
+    if (xhr.status === 200) {
+      chatMessages.innerHTML = xhr.responseText;
+      chatMessages.scrollTop = chatMessages.scrollHeight;
+    }
+  };
+  xhr.send();
+}
 
-  // Função para fechar a modal
-  function closeModal() {
-    var modal = document.getElementById("myModal");
-    modal.style.display = "none";
+function sendMessage() {
+  const messageInput = document.getElementById("message-input");
+  const message = messageInput.value.trim();
+  if (message !== "") {
+    // Enviar mensagem para o servidor usando AJAX
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", "send_message.php", true);
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.send("message=" + encodeURIComponent(message));
+    messageInput.value = "";
   }
+}
 
-  // Função para alternar a visibilidade das mensagens
-  function toggleMessages() {
-    const chatMessages = document.getElementById("chat-messages");
-    chatMessages.classList.toggle("hidden");
-  }
+// Função para abrir a modal
+function openModal() {
+  var modal = document.getElementById("myModal");
+  modal.style.display = "block";
+}
+
+// Função para fechar a modal
+function closeModal() {
+  var modal = document.getElementById("myModal");
+  modal.style.display = "none";
+}
+
+// Função para alternar a visibilidade das mensagens
+function toggleMessages() {
+  const chatMessages = document.getElementById("chat-messages");
+  chatMessages.classList.toggle("hidden");
+}
